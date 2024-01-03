25713cc89651718f6cea41bf5b87cae7e8f6c241
BITmun <pashabalko21@gmail.com>
2024-01-03 13:17:00 +0300

    feat: connect db

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const PORT = 5000;
+const db = require("./models");
 const corsOptions = {
   origin: "http://localhost:3000",
   credentials: true, //access-control-allow-credentials:true
@@ -12,10 +13,6 @@ app.use(express.json());
 
 app.use(cors(corsOptions));
 
-app.listen(PORT, () => {
-  console.log(`server listening on port ${PORT}`);
-});
-
 const authRouter = require("./routes/Auth");
 
 app.use("/auth", authRouter);
@@ -23,3 +20,10 @@ app.use("/auth", authRouter);
 const itemsRouter = require("./routes/Items");
 
 app.use("/items", itemsRouter);
+
+db.sequelize.sync().then(() => {
+  console.log("db connected successfully");
+  app.listen(PORT, () => {
+    console.log(`server listening on port ${PORT}`);
+  });
+});
